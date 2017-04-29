90ff5920cf164f6d0fe0e04f98f4ae83fb1dc67c
Nikola Bukovac <nikola.bukovac@outlook.com>
2017-04-29 11:35:02 +0200

    AES encrypt and decrypt added

---
+++ Labs/CryptoAlgorithms/Algorithms/AES.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using CryptoAlgorithms.Helpers;
+
+namespace CryptoAlgorithms.Algorithms
+{
+    public class AES
+    {
+
+        public static void Encrypt(string plainTextFilePath, string keyFilePath, string ivFilePath, 
+            string outputFilePath)
+        {
+            var key = GetKeyFromFile(keyFilePath);
+            var iv = GetIVFromFile(ivFilePath);
+            byte[] encrypted;
+
+            using (var aes = Aes.Create())
+            {
+                aes.IV = Converter.HexStringToBytes(iv);
+                aes.Key = Converter.HexStringToBytes(key);
+
+                var encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
+
+                using (var memoryStream = new MemoryStream())
+                {
+                    using (var cryptoStream = new CryptoStream(memoryStream, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (var writer = new StreamWriter(cryptoStream))
+                        {
+                            writer.Write(plainTextFilePath);
+                        }
+                    }
+
+                    encrypted = memoryStream.ToArray();
+                }
+            }
+
+            using (var writer = new StreamWriter(outputFilePath))
+            {
+                writer.Write(encrypted);
+            }
+        }
+
+        public static void Decrypt(string cipherTextFilePath, string keyFilePath, string ivFilePath,
+            string outputFilePath)
+        {
+            var key = GetKeyFromFile(keyFilePath);
+            var iv = GetIVFromFile(ivFilePath);
+            string plainText;
+
+            using (var aes = Aes.Create())
+            {
+                aes.IV = Converter.HexStringToBytes(iv);
+                aes.Key = Converter.HexStringToBytes(key);
+
+                var decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
+
+                using (var memoryStream = new MemoryStream())
+                {
+                    using (var cryptoStream = new CryptoStream(memoryStream, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (var reader = new StreamReader(cryptoStream))
+                        {
+                            plainText = reader.ReadToEnd();
+                        }
+                    }
+                }
+            }
+
+            using (var writer = new StreamWriter(outputFilePath))
+            {
+                writer.Write(plainText);
+            }
+        }
+
+        private static string GetIVFromFile(string ivFilePath)
+        {
+            string iv;
+            using (var reader = new StreamReader(ivFilePath))
+            {
+                iv = reader.ReadLine().Trim();
+            }
+            return iv;
+        }
+
+        private static string GetKeyFromFile(string keyFilePath)
+        {
+            string key;
+            using (var reader = new StreamReader(keyFilePath))
+            {
+                key = reader.ReadLine().Trim();
+            }
+            return key;
+        }
+    }
+}
---
+++ Labs/CryptoAlgorithms/Helpers/Converter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptoAlgorithms.Helpers
+{
+    public class Converter
+    {
+
+        public static byte[] HexStringToBytes(string hexString)
+        {
+            return Enumerable.Range(0, hexString.Length)
+                .Where(x => x % 2 == 0)
+                .Select(x => System.Convert.ToByte(hexString.Substring(x, 2), 16))
+                .ToArray();
+        }
+
+        public static string ByteArrayToString(byte[] bytes)
+        {
+            var hex = BitConverter.ToString(bytes);
+            return hex.Replace("-", "");
+        }
+    }
+}
