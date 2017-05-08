8e49be12e1fda2f32375cb8e46d044ba4d9fd584
Nikola Bukovac <nikola.bukovac@outlook.com>
2017-05-08 16:22:34 +0200

    Custom SHA-1 hash calculation

--- Labs/CryptoAlgorithms/Algorithms/CustomSHA1.cs
+++ Labs/CryptoAlgorithms/Algorithms/CustomSHA1.cs
@@ -1,17 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using CryptoAlgorithms.Helpers;
 
 namespace CryptoAlgorithms.Algorithms
 {
     public class CustomSHA1
     {
-
         private const int BlockSizeBytes = 64;
         private const int ByteShift = 8;
+        private const int BigArraySizeBytes = 80;
+        private const int WordsInBlock = 16;
 
         private const uint Constant0 = 0x5A827999;
         private const uint Constant1 = 0x6ED9EBA1;
@@ -24,37 +22,114 @@ namespace CryptoAlgorithms.Algorithms
         private const uint Hash3 = 0x10325476;
         private const uint Hash4 = 0xC3D2E1F0;
 
+        // (B AND C) OR ((NOT B) AND D)
+        private static readonly Func<uint, uint, uint, uint> F1Func = (b, c, d) => (b & c) | (~b & d);
+        // B XOR C XOR D
+        private static readonly Func<uint, uint, uint, uint> F2Func = (b, c, d) => b ^ c ^ d;
+        // (B AND C) OR (B AND D) OR (C AND D)
+        private static readonly Func<uint, uint, uint, uint> F3Func = (b, c, d) => (b & c) | (b & d) | (c & d);
+
+
         public static void CalculateHash(string plainTextFile, string hashOutputFilePath)
         {
             var paddedInput = PadInput(plainTextFile);
             var blocks = CreateBlocks(paddedInput);
+            var hash = new[] {Hash0, Hash1, Hash2, Hash3, Hash4};
+
+            GetHash(blocks, hash);
+
+            var hashString = Converter.UIntArrayToString(hash);
+        }
+
+        private static void GetHash(IEnumerable<uint[]> blocks, uint[] hash)
+        {
+            uint[] w = new uint[BigArraySizeBytes];
+
+            foreach (var block in blocks)
+            {
+                for (var t = 0; t < WordsInBlock; t++)
+                {
+                    w[t] = block[t];
+                }
+
+                for (var t = WordsInBlock; t < 80; t++)
+                {
+                    w[t] = RotateLeft(1, w[t - 3] ^ w[t - 8] ^ w[t - 14] ^ w[t - 16]);
+                }
+
+                var A = hash[0];
+                var B = hash[1];
+                var C = hash[2];
+                var D = hash[3];
+                var E = hash[4];
+
+                for (var i = 0; i < 80; i++)
+                {
+                    uint temp = 0;
+
+                    if (i < 20)
+                    {
+                        temp = RotateLeft(5, A) + F1Func(B, C, D) + E + w[i] + Constant0;
+                    }
+                    else if (i < 40)
+                    {
+                        temp = RotateLeft(5, A) + F2Func(B, C, D) + E + w[i] + Constant1;
+                    }
+                    else if (i < 60)
+                    {
+                        temp = RotateLeft(5, A) + F3Func(B, C, D) + E + w[i] + Constant2;
+                    }
+                    else
+                    {
+                        temp = RotateLeft(5, A) + F2Func(B, C, D) + E + w[i] + Constant3;
+                    }
+
+                    E = D;
+                    D = C;
+                    C = RotateLeft(30, B);
+                    B = A;
+                    A = temp;
+                }
+
+                hash[0] += A;
+                hash[1] += B;
+                hash[2] += C;
+                hash[3] += D;
+                hash[4] += E;
+            }
         }
 
         private static IEnumerable<uint[]> CreateBlocks(uint[] paddedInput)
         {
             var blocks = new List<uint[]>();
-            var temp = new uint[BlockSizeBytes];
+            var temp = new uint[WordsInBlock];
 
-            for (int i = 0; i < paddedInput.Length; i++)
+            for (var i = 0; i < paddedInput.Length; i++)
             {
-                temp[i%BlockSizeBytes] = paddedInput[i];
+                temp[i%WordsInBlock] = paddedInput[i];
 
-                if (i % BlockSizeBytes == BlockSizeBytes - 1)
+                if (i%WordsInBlock == WordsInBlock - 1)
                 {
                     blocks.Add(temp);
-                    temp = new uint[BlockSizeBytes];
+                    temp = new uint[WordsInBlock];
                 }
             }
 
             return blocks;
         }
 
+        private static uint RotateLeft(int bits, uint word)
+        {
+            return (word << bits) | (word >> (32 - bits));
+        }
+
+
         private static uint[] PadInput(string filePath)
         {
             var paddedBytes = PadInputBytes(filePath);
-            uint[] paddedInput = new uint[paddedBytes.Length / 4];
+            var paddedInput = new uint[paddedBytes.Length/4];
 
-            for (int i = 0; i < paddedBytes.Length; i += 4)
+            for (var i = 0; i < paddedBytes.Length; i += 4)
             {
                 uint temp = 0;
 
@@ -79,7 +154,7 @@ namespace CryptoAlgorithms.Algorithms
         {
             var inputBytes = FileOperations.ReadFromBinaryFile(filePath);
             var bytesToPad = Convert.ToUInt32((BlockSizeBytes - inputBytes.Length%BlockSizeBytes)%BlockSizeBytes);
-            byte[] paddedInput = new byte[inputBytes.Length + bytesToPad];
+            var paddedInput = new byte[inputBytes.Length + bytesToPad];
 
             if (bytesToPad == 0)
             {
@@ -87,14 +162,14 @@ namespace CryptoAlgorithms.Algorithms
             }
             else
             {
-                for (int i = 0; i < inputBytes.Length; i++)
+                for (var i = 0; i < inputBytes.Length; i++)
                 {
                     paddedInput[i] = inputBytes[i];
                 }
 
                 paddedInput[inputBytes.Length] = 0x80;
 
-                for (int i = 1; i < bytesToPad - 2; i++)
+                for (var i = 1; i < bytesToPad - 2; i++)
                 {
                     paddedInput[inputBytes.Length + i] = 0;
                 }
@@ -108,8 +183,7 @@ namespace CryptoAlgorithms.Algorithms
 
         private static byte GetByte(uint x, int n)
         {
-            return (byte)((x >> 8 * n) & 0xFF);
+            return (byte) ((x >> (8*n)) & 0xFF);
         }
-
     }
 }
--- Labs/CryptoAlgorithms/Helpers/Converter.cs
+++ Labs/CryptoAlgorithms/Helpers/Converter.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.Text;
 
 namespace CryptoAlgorithms.Helpers
 {
@@ -17,6 +18,23 @@ namespace CryptoAlgorithms.Helpers
         {
             var hex = BitConverter.ToString(bytes);
             return hex.Replace("-", "");
+        }
+
+        public static string UIntArrayToString(uint[] data)
+        {
+            var sb = new StringBuilder();
+
+            foreach (var value in data)
+            {
+                var top = value >> 24;
+                var midHigh = (value << 8) >> 24;
+                var midLow = (value << 16) >> 24;
+                var low = value & 0xFF;
+
+                sb.Append(top.ToString("X2") + midHigh.ToString("X2") + midLow.ToString("X2") + low.ToString("X2"));
+            }
+
+            return sb.ToString();
         }
     }
 }
